c1154f96e5384d7e5ad6466573e167b310f0201a
veretilo_sergei <sergey.veretilo@gmail.com>
2013-05-16 16:43:57 +0300

    Try validation errors

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -10,14 +10,23 @@ class UserController extends Zend_Controller_Action
 
     public function registerAction()
     {
+        $translate = include(APPLICATION_PATH . '/forms/translate.php');
+       //require_once APPLICATION_PATH . '/forms/translate.php';
+//echo APPLICATION_PATH . '/../forms/translate.php'; exit;
+        //require_once '/forms/translate.php';
+        
         $this->_helper->layout->setLayout('layout1');
         $this->view->headScript()->appendFile('/js/init_register.js');
         $this->view->headLink()->appendStylesheet('/css/init_register.css');
         
         $this->view->title = "Регистрация нового пользователя.";
         $this->view->headTitle($this->view->title, 'PREPEND');
+        
+        
+        $translator = new Zend_Translate_Adapter_Array($data);
         $form = new Application_Form_Register();
-
+        $form->setTranslator($translator);
+        
         if ($this->getRequest()->isPost()) {
                 if ($form->isValid($this->getRequest()->getPost())) {
                         $user = new Application_Model_Users();
