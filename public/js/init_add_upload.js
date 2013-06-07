d99fe0b015db35d2df35c656a91f186017a468b7
veretilo_sergei <sergey.veretilo@gmail.com>
2013-06-07 10:34:28 +0300

    fixed3

--- public/js/init_add_upload.js
+++ public/js/init_add_upload.js
@@ -1,17 +1,4 @@
 $(function() {
-<<<<<<< HEAD
-    
-    
-    $("#file_upload").uploadify({
-        height        : 30,
-        swf           : 'uploadify.swf',
-        uploader      : 'uploadify.php',
-        width         : 120
-=======
-    $('#file_upload').uploadify({
-        'swf'      : 'uploadify.swf',
-        'uploader' : 'uploadify.php'
-        // Put your options here
->>>>>>> c069b58ab05f0cf06a9afca45905beb432d2568a
-    });
+
+
 });
